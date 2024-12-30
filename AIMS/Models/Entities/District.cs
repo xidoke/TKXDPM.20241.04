55d5256d7953f98c6a5d96576d9367e78801f7c6
LeHaAnhDuc <duc.lha215351@sis.hust.edu.vn>
2024-12-30 15:19:07 +0700

    Delete folder

--- AIMS/Models/Entities/District.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AIMS.Models.Entities
-{
-    public class District
-    {
-        public string Id { get; set; }  
-        public string Name { get; set; }  
-        public string ProvinceId { get; set; }
-    }
-}
