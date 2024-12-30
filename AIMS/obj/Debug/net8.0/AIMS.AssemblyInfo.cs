4df9c8f71107754d9c9b9c09d3e74269579afcb7
LeHaAnhDuc <duc.lha215351@sis.hust.edu.vn>
2024-12-30 19:20:19 +0700

    Fix money currency

--- AIMS/obj/Debug/net8.0/AIMS.AssemblyInfo.cs
+++ AIMS/obj/Debug/net8.0/AIMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c3830d0f427337ec1b1f88c59c06f1950356f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e65cdb64573ea199c03f2fbb7998d6548de5ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
