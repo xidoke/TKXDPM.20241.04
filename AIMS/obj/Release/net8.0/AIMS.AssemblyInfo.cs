72c944cfed6a9d259092ce044127051eacd0dedc
Phạm Quốc Đạt <pqd21022003@gmail.com>
2025-01-01 20:32:14 +0700

    login - register page
    1

--- AIMS/obj/Release/net8.0/AIMS.AssemblyInfo.cs
+++ AIMS/obj/Release/net8.0/AIMS.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIMS")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14cb9e66a57fccc679bb26352d96ee312990974")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIMS")]
