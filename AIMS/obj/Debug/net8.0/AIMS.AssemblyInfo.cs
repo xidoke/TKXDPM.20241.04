a7d58e9834750be6f03afb5ac8a43a88fe84c0d7
datzxje <tudabest12@gmail.com>
2025-01-04 21:58:10 +0700

    update

--- AIMS/obj/Debug/net8.0/AIMS.AssemblyInfo.cs
+++ AIMS/obj/Debug/net8.0/AIMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76272a2ea7fe3effe22139a5e8fe99b1a1d5f4c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64db6a30f63a6294d9cfe1887c164814efadacd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
